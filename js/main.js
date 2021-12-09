ab7425d7d656e4c5ab209f976cc982f72cbb35fb
Nikita Chernov <chernovn152@gmail.com>
2021-12-09 15:04:57 +0300

    less_2

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ class ProductList{
         this.goods = [];
         this._fetchProducts();
         this.render();//вывод товаров на страницу
+        this.sumGoodSList();
     }
     _fetchProducts(){
         this.goods = [
@@ -14,6 +15,11 @@ class ProductList{
         ];
     }
     
+    sumGoodSList(){
+        let sum = Number();
+        this.goods.forEach((item) => sum += item.price);
+    }
+
     render(){
         const block = document.querySelector(this.container);
         for(let product of this.goods){
@@ -22,6 +28,58 @@ class ProductList{
 //           block.innerHTML += item.render();
         }
     }
+}
+
+class Basket {
+    
+    constructor(){
+        this.basket = [] //массив, объектов находящийся в корзине
+    }
+    
+    //создает блок .basket и добавляет его на страницу. Изначально корзина скрыта и имеет свойство position: absolute а родитель relative
+    createBasket(){
+
+    }
+
+    //метод будет назначает обработчик событий на кнопку корзины, при срабатывании события, класс .active добавляеться или удаляеться к блоку .basket . Будет использоваться classList.toggle(). Окно корзины будет показываться или скрываться.
+    showBasket(){
+        
+    }
+
+    //метод проверяет наличие совпадений в массиве this.basket = [], если совпадений нет, то добавляет товар в массив.
+    productInBasket(){
+        // new BasketItem() - будет возвращен объект товара со свойствами
+    }
+
+    //метод берет данные из массива с объектовами товаров, переберает их и через шаблонный литерал создает строку с HTML разметкой и она вставляется в блок корзины .basket
+    renderBasket(){
+        //summBasket()
+    }
+
+    //добавялет обработчик событий input на каждый элемент количества товара, при его изменении будет меняться сумма за конкретный товар и так же измняться Итоговая сумма
+    //** В ДЗ  Указанно "Добавьте для GoodsList метод, определяющий суммарную стоимость всех товаров." 
+    //Но несовсем было понятоно, ведь класс ProductList относиться к отрисовке товара, а не  к товару который мы добавили в корзину, ведь товаров может быть 1000
+    // а добавить можем только 3 штуки
+    summBasket(){
+
+    }
+}
+
+
+
+class BasketItem{
+
+    // метод назначает на кнопки карточек товара обработчик события 'click', при клике должен вызывать функцию createBasketItem();
+    actionItemBasket(){
+
+    }
+
+    // метод создает и возвращает готовый объект с данными о товаре, в которм будет цена, название, описание и т.д. 
+    //данные будут браться из data-атрибутов которые будут ли бо у кнопки данного товара или у самой карточки товара
+    createBasketItem(){
+
+    }
+
 }
 
 class ProductItem{
@@ -43,7 +101,7 @@ class ProductItem{
 
 let list = new ProductList();
 
-
+let basket = new Basket();
 
 //const products = [
 //    {id: 1, title: 'Notebook', price: 2000},
